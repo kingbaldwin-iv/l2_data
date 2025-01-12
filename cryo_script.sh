d61483d59eca92d0cfd5ba9304635c42127ef513
0xb <113536612+kingbaldwin-iv@users.noreply.github.com>
2025-01-12 06:33:30 -0800

    Update cryo_script.sh

--- cryo_script.sh
+++ cryo_script.sh
@@ -1,6 +1,7 @@
+# example v3 swap logs fetching 
 cryo logs \
     --label <LABEL> \
-    --blocks 114200000:130045411 \
+    --blocks <START_BLOCK>:<END_BLOCK> \
     --event-signature "Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)" \
     --topic0 0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67 \
     --output-dir <OUTPUT_DIR_NAME> \
